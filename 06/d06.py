3ab6bccbb2f91b328df86078575a377000c9da7b
Bohdan Widla <bohdan@bohdanwidla.pl>
2020-12-06 11:27:25 +0100

    Rewrite D6 pt 2: set intersect. instead of Counter

--- 06/d06.py
+++ 06/d06.py
@@ -1,18 +1,12 @@
-from collections import Counter
+from functools import reduce
 
 print("Day 6 of Advent of Code!")
 
 def count_anyone(form):
     return len(set(form.replace('\n', '')))
 
-def count_everyone(forms):
-    res = 0
-    for group in forms:
-        num_forms = len(group.split('\n'))
-        all_forms = ''.join(group.replace('\n', ''))
-        c = Counter(all_forms)
-        res += sum([1 for k in c if c[k] == num_forms])
-    return res
+def count_everyone(form):
+    return len(reduce(lambda a, b: a & b, (set(x) for x in form.split('\n'))))
 
 test_forms = '''abc
 
@@ -32,12 +26,12 @@ b'''
 
 print("Tests...")
 forms = test_forms.split('\n\n')
-print("Sum of YES answers in test forms:", sum([count_anyone(form) for form in forms]) == 11)
-print("Sum of ALL YES answers in forms:", count_everyone(forms) == 6)
+print("Sum of ANY YES answers in test forms:", sum([count_anyone(form) for form in forms]) == 11)
+print("Sum of ALL YES answers in forms:", sum([count_everyone(form) for form in forms]) == 6)
 print("---------------------")
 
 with open("input", mode = 'r') as inp:
     print("Solution...")
     forms = inp.read().split('\n\n')
     print("Sum of ANY YES answers in forms:", sum([count_anyone(form) for form in forms]))
-    print("Sum of ALL YES answers in forms:", count_everyone(forms))
+    print("Sum of ALL YES answers in forms:", sum([count_everyone(form) for form in forms]))
