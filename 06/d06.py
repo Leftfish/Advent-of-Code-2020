def15d5221a1350d9e381a7dee7d8f1eff190f02
Bohdan Widla <bohdan@bohdanwidla.pl>
2020-12-06 11:47:35 +0100

    Change Day 6 to one-liners

--- 06/d06.py
+++ 06/d06.py
@@ -2,11 +2,11 @@ from functools import reduce
 
 print("Day 6 of Advent of Code!")
 
-def count_anyone(form):
-    return len(set(form.replace('\n', '')))
+def count_anyone(forms):
+    return sum(len(set(form.replace('\n', ''))) for form in forms.split('\n\n'))
 
-def count_everyone(form):
-    return len(reduce(lambda a, b: a & b, (set(x) for x in form.split('\n'))))
+def count_everyone(forms):
+    return sum((len(reduce(lambda a, b: a & b, (set(x) for x in form.split('\n')))) for form in forms.split('\n\n')))
 
 test_forms = '''abc
 
@@ -25,13 +25,12 @@ a
 b'''
 
 print("Tests...")
-forms = test_forms.split('\n\n')
-print("Sum of ANY YES answers in test forms:", sum([count_anyone(form) for form in forms]) == 11)
-print("Sum of ALL YES answers in forms:", sum([count_everyone(form) for form in forms]) == 6)
+print("Sum of ANY YES answers in test forms:", count_anyone(test_forms) == 11)
+print("Sum of ALL YES answers in test forms:", count_everyone(test_forms) == 6)
 print("---------------------")
 
 with open("input", mode = 'r') as inp:
+    forms = inp.read()
     print("Solution...")
-    forms = inp.read().split('\n\n')
-    print("Sum of ANY YES answers in forms:", sum([count_anyone(form) for form in forms]))
-    print("Sum of ALL YES answers in forms:", sum([count_everyone(form) for form in forms]))
+    print("Sum of ANY YES answers in forms:", count_anyone(forms))
+    print("Sum of ALL YES answers in forms:", count_everyone(forms))
