d5384d4c5104a7ee9f47b9861d6e281325ec03ed
Bohdan Widla <bohdan@bohdanwidla.pl>
2020-12-11 16:31:17 +0100

    Add alternative solution to Day 11.

--- 11/d11.py
+++ 11/d11.py
@@ -1,22 +1,9 @@
-from enum import Enum
-
 print('Day 11 of Advent of Code!')
 
 
 EMPTY = 'L'
 TAKEN = '#'
 FLOOR = '.'
-
-
-class Dirs(Enum):
-    UP = (-1, 0)
-    DWN = (1, 0)
-    RGT = (0, 1)
-    LFT = (0, -1)
-    UP_RGT = (-1, 1)
-    UP_LFT = (-1, -1)
-    DWN_RGT = (1, 1)
-    DWN_LFT = (1, -1)
 
 
 def make_map(lines):
@@ -30,23 +17,23 @@ def make_map(lines):
 
 
 def count_adjacent_p1(y, x, arr):
-    adjacent = 0
+    adjacent = []
     coords = [(y-1, x-1), (y-1, x), (y-1, x+1), (y, x-1), (y, x+1), (y+1, x-1), (y+1, x), (y+1, x+1)]
     for coord in coords:
         y, x = coord
         if arr[y][x] == TAKEN:
-            adjacent += 1
+            adjacent.append(coord)
     return adjacent
 
 
 def count_adjacent_p2(y, x, arr):
-    adjacent = 0
+    adjacent = []
     # UP
     dy = y
     while dy > 0:
         dy -= 1
         if arr[dy][x] == TAKEN:
-            adjacent += 1
+            adjacent.append((dy, x))
             break
         elif arr[dy][x] == EMPTY:
             break
@@ -55,7 +42,7 @@ def count_adjacent_p2(y, x, arr):
     while dy < len(arr)-1:
         dy += 1
         if arr[dy][x] == TAKEN:
-            adjacent += 1
+            adjacent.append((dy, x))
             break
         elif arr[dy][x] == EMPTY:
             break
@@ -64,7 +51,7 @@ def count_adjacent_p2(y, x, arr):
     while dx < len(arr[0])-1:
         dx += 1
         if arr[y][dx] == TAKEN:
-            adjacent += 1
+            adjacent.append((y, dx))
             break
         elif arr[y][dx] == EMPTY:
             break
@@ -73,7 +60,7 @@ def count_adjacent_p2(y, x, arr):
     while dx > 0:
         dx -= 1
         if arr[y][dx] == TAKEN:
-            adjacent += 1
+            adjacent.append((y, dx))
             break
         elif arr[y][dx] == EMPTY:
             break
@@ -83,7 +70,7 @@ def count_adjacent_p2(y, x, arr):
         dx += 1
         dy += 1
         if arr[dy][dx] == TAKEN:
-            adjacent += 1
+            adjacent.append((dy, dx))
             break
         elif arr[dy][dx] == EMPTY:
             break
@@ -94,7 +81,7 @@ def count_adjacent_p2(y, x, arr):
         dx += 1
         dy -= 1
         if arr[dy][dx] == TAKEN:
-            adjacent += 1
+            adjacent.append((dy, dx))
             break
         elif arr[dy][dx] == EMPTY:
             break
@@ -105,7 +92,7 @@ def count_adjacent_p2(y, x, arr):
         dx -= 1
         dy += 1
         if arr[dy][dx] == TAKEN:
-            adjacent += 1
+            adjacent.append((dy, dx))
             break
         elif arr[dy][dx] == EMPTY:
             break
@@ -116,33 +103,11 @@ def count_adjacent_p2(y, x, arr):
         dx -= 1
         dy -= 1
         if arr[dy][dx] == TAKEN:
-            adjacent += 1
+            adjacent.append((dy, dx))
             break
         elif arr[dy][dx] == EMPTY:
             break
 
-    return adjacent
-
-
-def is_inbound(y, x, arr):
-    return 0 < y < len(arr)-1 and 0 < x < len(arr[0])-1
-
-
-def count_adjacent_p2_alt(y, x, arr):
-    adjacent = 0
-    for direction in Dirs:
-        y_start = y
-        x_start = x
-        dy = direction.value[0]
-        dx = direction.value[1]
-        while is_inbound(y_start+dy, x_start+dx, arr):
-            y_start += dy
-            x_start += dx
-            if arr[y_start][x_start] == TAKEN:
-                adjacent += 1
-                break
-            elif arr[y_start][x_start] == EMPTY:
-                break
     return adjacent
 
 
@@ -153,11 +118,11 @@ def check_seats(arr, counter, limit):
         for x in range(len(arr[0])):
             if arr[y][x] == EMPTY:
                 adjacent = counter(y, x, arr)
-                if adjacent == 0:
+                if len(adjacent) == 0:
                     to_take.append((y, x))
             elif arr[y][x] == TAKEN:
                 adjacent = counter(y, x, arr)
-                if adjacent >= limit:
+                if len(adjacent) >= limit:
                     to_leave.append((y, x))
     return to_take, to_leave
 
@@ -195,12 +160,10 @@ L.LLLLL.LL'''
 
 t1 = make_map(test_layout.split('\n'))
 t2 = make_map(test_layout.split('\n'))
-t3 = make_map(test_layout.split('\n'))
 print('---------------')
 print('Tests...')
 print('Occupied seats, part 1:', simulate_seating(t1, count_adjacent_p1, 4) == 37)
-print('Occupied seats, part 2 (faster, less pretty code):', simulate_seating(t2, count_adjacent_p2, 5) == 26)
-print('Occupied seats, part 2 (slower, prettier code):', simulate_seating(t3, count_adjacent_p2_alt, 5) == 26)
+print('Occupied seats, part 2:', simulate_seating(t2, count_adjacent_p2, 5) == 26)
 print('---------------------')
 
 
@@ -209,7 +172,5 @@ with open('input', mode='r') as inp:
     test = [line.rstrip() for line in inp.readlines()]
     p1 = make_map(test)
     p2 = make_map(test)
-    p2_alt = make_map(test)
     print('Occupied seats, part 1:', simulate_seating(p1, count_adjacent_p1, 4))
-    print('Occupied seats, part 2 (faster, less pretty code):', simulate_seating(p2, count_adjacent_p2, 5))
-    print('Occupied seats, part 2 (slower, prettier code):', simulate_seating(p2_alt, count_adjacent_p2_alt, 5))
+    print('Occupied seats, part 2:', simulate_seating(p2, count_adjacent_p2, 5))
