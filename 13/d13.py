39191238abfc59ae3f7dd2a33a894e1d464ec8a9
Bohdan Widla <bohdan@bohdanwidla.pl>
2020-12-13 16:10:44 +0100

    Some more D13 refactoring.

--- 13/d13.py
+++ 13/d13.py
@@ -3,15 +3,16 @@ print('Day 13 of Advent of Code!')
 
 def check_next(timestamp, interval):
     time = ((timestamp + interval) // interval) * interval
-    return (interval, time - timestamp)
+    return interval, time - timestamp
 
 
 def find_first_bus(timestamp, intervals):
-    shortest = [float('inf'), float('inf')]
+    shortest = [0, 0]
     for interval in intervals:
-        bus_id, next_arrival = check_next(timestamp, interval)
-        if next_arrival < shortest[1]:
-            shortest[0], shortest[1] = bus_id, next_arrival
+        if interval != 'x':
+            bus_id, next_arrival = check_next(timestamp, interval)
+            if not shortest[1] or next_arrival < shortest[1]:
+                shortest[0], shortest[1] = bus_id, next_arrival
     return shortest[0] * shortest[1]
 
 
@@ -24,26 +25,46 @@ def check_congruence(to_check, test_stamp):
         if bus_id != 'x' and modified_stamp % bus_id != 0:
             success = False
             break
-
     return success
 
 
-def check(intervals):
+def find_bus_sequence(intervals):
+    '''
+    Just so I don't forget WTH this was all about:
+    - we assume that the first element in the input is a number, not an 'x'
+    - first we find the timestamp with congruence for the first two bus ids (0:end+1)
+    - we do this by checking all subsequent multiples of the first element
+    - once we get the result, we expand the search by one
+    - we start our search at the result...
+    - ...but new we jump by the multiple of all previous numbers.
+
+    E.g. for input [2, 3, 5, 7, 11]:
+
+    1. First we search for congruence for [2, 5], starting from 2, jump by 2
+    2. We find the congruence for timestamp 2.
+    3. Expand to [2, 3, 5], start from 2, now jump by 6 (2 * 3)
+    4. We find the congruence for timestamp 8.
+    5. Expand to [2, 3, 5, 7], start from 8, now jump by 30 (2 * 3 * 5)
+    6. We find the congruence for timestamp 158.
+    7. Expand to [2, 3, 5, 7, 11], start from 158, now jump by 210 (2 * 3 * 5 * 7)
+    8. We find the congruence for timestamp 788, we're done.
+    '''
+    
     end = 1
-    result = intervals[0] if intervals[0] != 'x' else 1
+    timestamp = intervals[0] if intervals[0] != 'x' else 1
     interval = intervals[0] if intervals[0] != 'x' else 1
     
     while end < len(intervals):
         while True:
-            if not check_congruence(intervals[:end+1], result):
-                result += interval
+            if not check_congruence(intervals[:end+1], timestamp):
+                timestamp += interval
             else:
                 if intervals[end] != 'x':
                     interval *= intervals[end]
                 break
         end += 1
 
-    return result
+    return timestamp
 
 
 test = '''939
@@ -51,19 +72,17 @@ test = '''939
 
 data = test.split('\n')
 timestamp = int(data[0])
-intervals = [int(i) for i in data[1].split(',') if i.isdigit()]
-all_intervals = [int(i) if i.isdigit() else i for i in data[1].split(',')]
+intervals = [int(i) if i.isdigit() else i for i in data[1].split(',')]
 
 print('Tests...')
 print('Next ID * time to wait:', find_first_bus(timestamp, intervals) == 295)
-print('First timestamp with sequence:', check(all_intervals) == 1068781)
+print('First timestamp with sequence:', find_bus_sequence(intervals) == 1068781)
 print('---------------------')
 
 with open('input', mode='r') as inp:
     print('Solution...')
     data = inp.read().split('\n')
     timestamp = int(data[0])
-    intervals = [int(i) for i in data[1].split(',') if i.isdigit()]
-    all_intervals = [int(i) if i.isdigit() else i for i in data[1].split(',')]
+    intervals = [int(i) if i.isdigit() else i for i in data[1].split(',')]
     print('Next ID * time to wait:', find_first_bus(timestamp, intervals))
-    print('First timestamp with sequence:', check(all_intervals))
+    print('First timestamp with sequence:', find_bus_sequence(intervals))
